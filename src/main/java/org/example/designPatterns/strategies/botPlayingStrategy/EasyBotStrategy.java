f2fb45ae0b55fc170be337be0f98646504b25807
sandeep-sowpati <letsmailsandeepsowpati@gmail.com>
2023-10-12 11:22:16 +0530

    completed startegies related to TicTacToe

--- src/main/java/org/example/designPatterns/strategies/botPlayingStrategy/EasyBotStrategy.java
+++ src/main/java/org/example/designPatterns/strategies/botPlayingStrategy/EasyBotStrategy.java
@@ -2,10 +2,22 @@ package org.example.designPatterns.strategies.botPlayingStrategy;
 
 import org.example.entities.Board;
 import org.example.entities.Cell;
+import org.example.entities.CellStatus;
+
+import java.util.List;
 
 public class EasyBotStrategy implements BotPlayingStrategy{
     @Override
     public Cell makemove(Board board) {
+
+        // return the first empty cell.
+        for (List<Cell> row: board.getBoard()) {
+            for (Cell cell: row) {
+                if (cell.getCellStatus().equals(CellStatus.EMPTY)) {
+                    return cell;
+                }
+            }
+        }
         return null;
     }
 }
